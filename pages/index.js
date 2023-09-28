b926d5751e9084382854beb8a1aeac22202edbdd
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-09-27 21:04:53 -0400

    added links

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
+import Link from 'next/link';
 import Head from 'next/head';
+
+import Date from '../components/date';
 import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
 import Layout, { siteTitle } from '../components/layout';
-
-export async function getStaticProps() {
-	const allPostsData = getSortedPostsData();
-	return {
-		props: {
-			allPostsData
-		}
-	};
-}
 
 export default function Home({ allPostsData }) {
 	return (
@@ -35,15 +29,24 @@ export default function Home({ allPostsData }) {
 				<ul className={utilStyles.list}>
 					{allPostsData.map(({ id, date, title }) => (
 						<li className={utilStyles.listItem} key={id}>
-							{title}
+							<Link href={`/posts/${id}`}>{title}</Link>
 							<br />
-							{id}
-							<br />
-							{date}
+							<small className={utilStyles.lightText}>
+								<Date dateString={date} />
+							</small>
 						</li>
 					))}
 				</ul>
 			</section>
 		</Layout>
 	);
+}
+
+export async function getStaticProps() {
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData
+		}
+	};
 }
